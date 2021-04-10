a91e431896825fb0eae8c091ac8277e0f48489ae
shivashanmugam <sivashanmugam4444@gmail.com>
2021-04-10 11:50:19 +0530

    port updated from 8080 to 3000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //module delcar
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var server = app.listen(port, function (err) {
